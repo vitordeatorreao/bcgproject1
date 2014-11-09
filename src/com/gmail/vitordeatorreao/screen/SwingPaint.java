7995311ae825b0e760f84bda3a0f98785d6b2e0a
Vitor de Albuquerque Torreao <vitordeatorreao@gmail.com>
2014-11-09 13:01:02 -0300

    Added a MenuBar to open files

--- src/com/gmail/vitordeatorreao/screen/SwingPaint.java
+++ src/com/gmail/vitordeatorreao/screen/SwingPaint.java
@@ -1,10 +1,18 @@
 package com.gmail.vitordeatorreao.screen;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import com.gmail.vitordeatorreao.scene.NonConformantSceneFile;
@@ -27,6 +35,9 @@ public class SwingPaint {
 	
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 500;
+	
+	private static JFrame frame;
+	private static PaintablePanel paintablePanel;
 
 	/**
 	 * The main loop of the program.
@@ -35,18 +46,18 @@ public class SwingPaint {
 	 */
 	public static void main(String[] args) {
 		
-		File file = new File("vaso.byu");
-		
-		if(args.length > 0) {
-			file = new File(args[0]);
-		}
-		
-		try {
-			SceneController.getInstance().loadScene(file);
-		} catch (IOException | NonConformantSceneFile e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		File file = new File("vaso.byu");
+//		
+//		if(args.length > 0) {
+//			file = new File(args[0]);
+//		}
+//		
+//		try {
+//			SceneController.getInstance().loadScene(file);
+//		} catch (IOException | NonConformantSceneFile e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			
@@ -64,11 +75,77 @@ public class SwingPaint {
 	 */
 	private static void createAndShowGUI() {
 		
-		JFrame f = new JFrame("Vitor Torreao CG Project");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(WIDTH, HEIGHT);
-        f.add(new PaintablePanel(), BorderLayout.CENTER);
-        f.setVisible(true);
+		frame = new JFrame("Vitor Torreao CG Project");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        
+        //Added Paintable Panel
+        paintablePanel = new PaintablePanel();
+        frame.add(paintablePanel, BorderLayout.CENTER);
+        
+        //Create menu bar
+        JMenuBar menuBar = new JMenuBar();
+        ImageIcon openIcon;
+        ImageIcon exitIcon;
+        try {
+        	openIcon = new ImageIcon(SwingPaint.class.getResource(
+        			"/com/gmail/vitordeatorreao/images/open.png"));
+        	exitIcon = new ImageIcon(SwingPaint.class.getResource(
+        			"/com/gmail/vitordeatorreao/images/exit.png"));
+        } catch (Exception e){
+        	openIcon = new ImageIcon("open.png");
+        	exitIcon = new ImageIcon("exit.png");
+        }
+        
+        //Create Open File Menu Item
+        JMenu file = new JMenu("File");
+        JMenuItem openMenuItem = new JMenuItem("Open", openIcon);
+        openMenuItem.setToolTipText("Load objects from BYU files");
+        openMenuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser openFile = new JFileChooser();
+				int returnVal = openFile.showOpenDialog(null);
+				
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = openFile.getSelectedFile();
+					try {
+						SceneController.getInstance().loadScene(file);
+						paintablePanel.repaint();
+					} catch (IOException | NonConformantSceneFile e) {
+						JOptionPane.showMessageDialog(frame, 
+								e.getMessage(), "Error while opening file", 
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				
+			}
+		});
+        
+        //Create Exit program menu item
+        JMenuItem exitMenuItem = new JMenuItem("Exit", exitIcon);
+        exitMenuItem.setToolTipText("Exit application");
+        exitMenuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+				
+			}
+		});
+        
+        //Add menu items to Menu
+        file.add(openMenuItem);
+        file.add(exitMenuItem);
+        
+        //Add menu to menu bar
+        menuBar.add(file);
+        
+        //Set the JFrame MenuBar to the menu bar created
+        frame.setJMenuBar(menuBar);
+        
+        frame.setVisible(true);
 		
 	}
 
